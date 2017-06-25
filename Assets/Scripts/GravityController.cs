72a777fd9c169c270750733de4130547b178e819
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-06-25 17:54:40 +0900

    実機で動作する処理を作成

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -20,15 +20,24 @@ public class GravityController : MonoBehaviour
 	void Update ()
 	{
 		Vector3 vector = new Vector3 ();
-		// キーの入力を検知しベクトルを設定
-		vector.x = Input.GetAxis ("Horizontal");
-		vector.z = Input.GetAxis ("Vertical");
 
-		// 高さ方向の判定はキーのzとする
-		if (Input.GetKey ("z")) {
-			vector.y = 1.0f;
+		// Unityエディターと実機で処理を分ける
+		if (Application.isEditor) {
+			// キーの入力を検知しベクトルを設定
+			vector.x = Input.GetAxis ("Horizontal");
+			vector.z = Input.GetAxis ("Vertical");
+
+			// 高さ方向の判定はキーのzとする
+			if (Input.GetKey ("z")) {
+				vector.y = 1.0f;
+			} else {
+				vector.y = -1.0f;
+			}
 		} else {
-			vector.y = -1.0f;
+			// 加速度センサーの入力をUnity空間の軸にマッピングする
+			vector.x = Input.acceleration.x;
+			vector.z = Input.acceleration.y;
+			vector.y = Input.acceleration.z;
 		}
 
 		// シーンの重力を入力ベクトルの方向に合わせて変化させる
